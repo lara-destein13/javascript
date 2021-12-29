89fa9a3bda6b79d7ca696c26e22c30a491a77be3
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-28 21:26:25 -0500

    learn about objects

--- objects/index.js
+++ objects/index.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+console.log("-- print hello 1 time");
+console.log("hello");
+
+// create an empty object
+console.log("--create an empty object");
+let myObject = {};
+let myString = JSON.stringify(myObject);
+console.log(myString);
+
+// create a non-empty object
+console.log("--create a non-empty object");
+myObject = {
+    age: 1,
+    name: "Luna",
+}
+myString = JSON.stringify(myObject);
+console.log(myString);
+
+// get a value given a literal key using square bracket notation
+console.log("--get a value given a literal key using square bracket notation");
+age = myObject["age"];
+console.log(age);
+
+
+// get a value given a literal key using dot notation
+console.log("--get a value given a literal key using dot notation");
+age = myObject.age;
+console.log(age);
+
+// get a value given a variable that contains a key
+console.log("--get a value given a variable that contains a key");
+myKey = "age";
+age = myObject.myKey;
+console.log(age);
